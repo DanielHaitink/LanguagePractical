690e17f338ee5828fdb173b66a62847d99e2ee0d
DanielsWrath <danielhaitink@gmail.com>
2016-05-23 15:49:43 +0200

    even more variables and expected answer

--- prePostParser.py
+++ prePostParser.py
@@ -1,7 +1,13 @@
 import socket, sys
 import variables as v
 from lxml import etree
+from questionParser import getTreeWordList
 
+def containsFromList(sentence, list):
+	for item in list:
+		if item in sentence:
+			return True
+	return False
 
 # parse input sentence and return alpino output as an xml element tree
 def parseSentenceAlpino(sent, host='zardoz.service.rug.nl', port=42424):
@@ -22,7 +28,29 @@ def parseSentenceAlpino(sent, host='zardoz.service.rug.nl', port=42424):
 	return xml
 
 def preParseSentence(sentence):
+	expectedAnswer = v.ANSWER_UNKNOWN
+
 	alpinoXML = parseSentenceAlpino(sentence) 
+
+	whds = alpinoXML.xpath('//node[@rel="whd"]')
+	if whds == None or whds[0] == None:
+		if __DEBUG__:
+			print("ERROR: NO WHDS FOUND: "+ whds , file=sys.stderr)
+			return None
+	whd = getTreeWordList(whds[0], v.TYPE_WORD)
+
+	if containsFromList(whd, v.WHD_LOCATION):
+		expectedAnswer = v.ANSWER_LOCATION
+	elif containsFromList(whd, v.WHD_PERSON):
+		expectedAnswer = v.ANSWER_PERSON
+	elif containsFromList(whd, v.WHD_NUMBER):
+		expectedAnswer = v.ANSWER_NUMBER
+	elif containsFromList(whd, v.WHD_DATE):
+		expectedAnswer = v.ANSWER_DATE
+
+	print(whd)
+
+
 	#determine what kind of sentence it is
 	#send sentence to correct function in questionParser, then check if output is as expected
 	#give both sentence as alpino xml, it only has to be parsed once (probably)
--- variables.py
+++ variables.py
@@ -9,3 +9,16 @@ TYPE_SENSE = "sense"
 TYPE_WORD = "word"
 TYPE_ROOT = "root"
 TYPE_LEMMA = "lemma"
+
+#Expected answer
+ANSWER_LOCATION = "location"
+ANSWER_PERSON = "person"
+ANSWER_DATE = "date"
+ANSWER_NUMBER = "number"
+ANSWER_UNKNOWN = None
+
+#WHD words for expected answer
+WHD_LOCATION = ["waar"]
+WHD_PERSON = ["wie"]
+WHD_DATE = ["wanneer"]
+WHD_NUMBER = ["hoeveel"]
