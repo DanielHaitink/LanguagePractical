1f6477c32183474a60f79748114513222e2cad61
jwdw <jwdwit@gmail.com>
2016-05-25 10:27:17 +0200

    worked on QuestionParser

--- questionParser.py
+++ questionParser.py
@@ -1,5 +1,31 @@
 import sys, re
 import variables as v
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+def findProperties(URI): #find all properties that a resource in dbpedia has.
+    query = """
+    SELECT ?property
+    WHERE {<%s> ?property ?value  }
+    """ % (URI)
+
+    sparql = SPARQLWrapper("http://nl.dbpedia.org/sparql")
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    raw_properties = []
+
+    for result in results["results"]["bindings"]:
+        for arg in result :
+            answer = result[arg]["value"]
+            raw_properties.append(answer)
+
+    properties = []
+    for element in raw_properties:
+        if "nl.dbpedia.org/property" in element:
+            element = element.split("/")
+            properties.append(element[-1])
+    return list(set(properties))
 
 #give alpino node and wordtype you want to extract see variables
 def getTreeWordList(xml, wordtype):
@@ -50,3 +76,35 @@ def makeAZ(string):
 
 #TODO: -for each type of question, add function to parse it
 #- send parsed information to correct SPARQL query template
+
+def parseXofY(xml, expectedAnswer):
+    #find concept
+    names = xml.xpath('//node[@rel="obj1" and ../@rel="mod"]')
+    for name in names:
+        concept = getTreeWordList(name,v.TYPE_LEMMA)
+    if concept==None or concept=="":
+        return None
+    #find property
+    names = xml.xpath('//node[@rel="hd" and ../@rel="su"]')
+    for name in names:
+        property = getTreeWordList(name,v.TYPE_LEMMA)
+    if property==None or property=="":
+        return None
+
+    #test
+    #return (property, concept)
+
+    #find URI of concept
+    URI = getDomainURI(concept)
+
+    #find properties of the concept
+    props = findProperties(concept)
+
+    #match properties using synonyms
+
+    
+
+    #check best match, return the first thing that matches expectedAnswer
+
+def parseNumberOf(xml, expectedAnswer):
+    return None
